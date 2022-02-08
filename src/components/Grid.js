3045ac7b558fca62fcb6448f82b60aeae3fb0bea
unknown <naghmouchiwala@gmail.com>
2022-02-08 20:44:17 +0100

    initial commit

---
+++ src/components/Grid.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Custombtn from './CustomBtn';
+import {Typography} from '@material-ui/core';
+import {makeStyles} from '@material-ui/core/styles';
+
+const styles = makeStyles({
+    wrapper: {
+       display: "flex",
+       flexDirection: "column", 
+       alignItems: "center", 
+       padding: "0 5rem 0 5rem"
+    }, 
+    item: {
+       paddingTop: "1rem"
+    }
+})
+
+function Grid(props) {
+    
+    const {icon, title, btnTitle} = props;
+    const classes = styles(); 
+    return (
+        <div className={classes.wrapper}>
+            <div className={classes.item}>{icon}</div>
+            <Typography className={classes.item} variant="h5">{title}</Typography>
+            <div className={classes.item}>
+                <Custombtn  txt={btnTitle}/>
+            </div>
+        </div>
+    )
+}
+
+export default Grid;
---
+++ src/components/Navbar.js
@@ -0,0 +1,74 @@
+import React from 'react'
+import CustomBtn from './CustomBtn'
+import logo from '../logo.svg'
+import logoMobile from '../logoMobile.svg'
+import {Toolbar, Typography} from '@material-ui/core'
+import {makeStyles} from "@material-ui/core/styles";
+
+
+
+const styles = makeStyles({
+    bar:{
+        paddingTop: "1.15rem",
+        backgroundColor: "#fff",
+        ['@media (max-width:780px)']: { 
+           flexDirection: "column"
+          }
+    },
+    logo: {
+        width: "15%", 
+        ['@media (max-width:780px)']: { 
+           display: "none"
+           }
+    },
+    logoMobile:{
+        width: "100%", 
+        display: "none", 
+        ['@media (max-width:780px)']: { 
+            display: "inline-block"
+            }
+    },
+    menuItem: {
+        cursor: "pointer", 
+        flexGrow: 1,
+        "&:hover": {
+            color:  "#4f25c8"
+        },
+        ['@media (max-width:780px)']: { 
+            paddingBottom: "1rem"    }
+    }
+});
+
+
+
+function Navbar() {
+    
+
+    const classes = styles();
+    return (
+            <Toolbar position="sticky" color="rgba(0, 0, 0, 0.87)" className={classes.bar}>   
+                <img src={logo} className={classes.logo}/> 
+                <img src={logoMobile} className={classes.logoMobile}/> 
+                <Typography variant="h6" className={classes.menuItem}>
+                   Home
+                </Typography>
+                <Typography variant="h6" className={classes.menuItem}>
+                   About
+                </Typography>
+                <Typography variant="h6" className={classes.menuItem}>
+                   Donate
+                </Typography>
+               
+                <Typography variant="h6" className={classes.menuItem}>
+                   Events
+                </Typography>
+                <Typography variant="h6" className={classes.menuItem}>
+                   Job Opportunities
+                </Typography>
+               
+                <CustomBtn txt="Be With Us"/>
+            </Toolbar>
+    );
+};
+
+export default Navbar;
